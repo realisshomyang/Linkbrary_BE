7fd9de92e7a9a282ecd166802e91eae1f4dc8440
realisshomyang <realisshoman@kakao.com>
2024-05-05 13:50:31 +0900

    feat : UserDirectory 조회 구현

--- src/main/java/com/linkbrary/domain/directory/controller/UserDirectoryController.java
+++ src/main/java/com/linkbrary/domain/directory/controller/UserDirectoryController.java
@@ -6,6 +6,7 @@ import com.linkbrary.domain.directory.dto.CreateUserDirectoryRequestDTO;
 import com.linkbrary.domain.directory.dto.UserDirectoryResponseDTO;
 import com.linkbrary.domain.directory.service.UserDirectoryService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -21,5 +22,11 @@ public class UserDirectoryController {
     @PostMapping
     public ApiResponse<UserDirectoryResponseDTO> createDirectory(@Valid @RequestBody CreateUserDirectoryRequestDTO createUserDirectoryRequestDTO) {
         return ApiResponse.onSuccess(userDirectoryService.createDirectory(createUserDirectoryRequestDTO));
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "해당 디렉토리 조회 api (폴더 몇개, 링크)")
+    public ApiResponse<UserDirectoryResponseDTO> getDirectory(@Parameter(description = "조회할 디렉토리의 ID")  @PathVariable Long id) {
+        return ApiResponse.onSuccess(userDirectoryService.getDirectory(id));
     }
 }
--- src/main/java/com/linkbrary/domain/directory/service/UserDirectoryService.java
+++ src/main/java/com/linkbrary/domain/directory/service/UserDirectoryService.java
@@ -5,28 +5,23 @@ import com.linkbrary.common.response.ApiResponse;
 import com.linkbrary.common.response.code.ErrorCode;
 import com.linkbrary.common.response.exception.handler.UserDirectoryHandler;
 import com.linkbrary.domain.directory.dto.CreateUserDirectoryRequestDTO;
-import com.linkbrary.domain.directory.dto.UpdateDirectoryNameRequestDTO;
-import com.linkbrary.domain.directory.dto.UpdateUserDirectoryLocationRequestDTO;
 import com.linkbrary.domain.directory.dto.UserDirectoryResponseDTO;
 import com.linkbrary.domain.directory.entity.UserDirectory;
 import com.linkbrary.domain.directory.repository.UserDirectoryRepository;
-import com.linkbrary.domain.link.repository.UserLinkRepository;
 import com.linkbrary.domain.user.entity.Member;
 import com.linkbrary.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
 @RequiredArgsConstructor
 public class UserDirectoryService {
+
     private final UserDirectoryRepository userDirectoryRepository;
     private final UserService userService;
 
+    @Transactional
     public UserDirectoryResponseDTO createDirectory(CreateUserDirectoryRequestDTO createUserDirectoryRequestDTO) {
         Member member = userService.getMemberFromToken();
         Long parentFolderId = createUserDirectoryRequestDTO.getParentFolderId();
@@ -47,6 +42,12 @@ public class UserDirectoryService {
                     .build();
         }
         userDirectoryRepository.save(userDirectory);
+        return UserDirectoryResponseDTO.from(userDirectory);
+    }
+
+    @Transactional(readOnly = true)
+    public UserDirectoryResponseDTO getDirectory(Long id) {
+        UserDirectory userDirectory = userDirectoryRepository.findById(id).orElseThrow(() -> new UserDirectoryHandler(ErrorCode.DIRECTORY_NOT_FOUND));
         return UserDirectoryResponseDTO.from(userDirectory);
     }
 }
