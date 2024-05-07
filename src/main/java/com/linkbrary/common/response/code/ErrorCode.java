fc531faa74a8ea225d264cb87156abba84d99579
realisshomyang <realisshoman@kakao.com>
2024-05-07 17:46:15 +0900

    feat : ErrorCode 작성

--- src/main/java/com/linkbrary/common/response/code/ErrorCode.java
+++ src/main/java/com/linkbrary/common/response/code/ErrorCode.java
@@ -14,8 +14,27 @@ public enum ErrorCode implements BaseErrorCode {
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다."),
     FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
     UNSUPPORTED_MEDIA_TYPE(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "COMMON405", "지원하지 않는 미디어 파일입니다."),
-    IMAGE_IS_NULL(HttpStatus.BAD_REQUEST, "COMMON406", "이미지 파일은 null이면 안됩니다.")
-    ,;
+    IMAGE_IS_NULL(HttpStatus.BAD_REQUEST, "COMMON406", "이미지 파일은 null이면 안됩니다."),
+
+    // 멤버 관련 에러
+    MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "MEMBER4001", "사용자가 없습니다."),
+    MEMBER_LOGIN_FAILURE(HttpStatus.BAD_REQUEST, "MEMBER4003", "아이디 혹은 비밀번호를 잘못 입력하였습니다."),
+    NICKNAME_NOT_EXIST(HttpStatus.BAD_REQUEST, "MEMBER4002", "닉네임은 필수 입니다."),
+    MEMBER_SIGNUP_ERROR(HttpStatus.BAD_REQUEST, "SIGNUP4001", "회원가입 유효성 검사 실패"),
+    EMAIL_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "SIGNUP4002", "이미 존재하는 이메일입니다."),
+    //링크 관련 에러
+    LINK_NOT_FOUND(HttpStatus.NOT_FOUND, "LINK4041", "디비에 저장된 링크가 존재하지 않습니다."),
+    LINK_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "LINK4001", "해당 링크는 지원되지 않습니다."),
+    LINK_ALREADY_EXIST(HttpStatus.BAD_REQUEST,"LINK4002","해당 링크는 이미 저장되어 있습니다."),
+
+    //디렉토리 관련 에러
+    DIRECTORY_NOT_FOUND(HttpStatus.NOT_FOUND, "DIRECTORY4041", "해당 디렉토리가 존재하지 않습니다."),
+    USER_REMINDER_SETTING_NOT_FOUND(HttpStatus.NOT_FOUND,"REMINDER4041", "유저의 리마인더 설정이 존재하지 않습니다."),
+
+    //API call 관련 에러
+    API_GET_CALL_MODE_ERROR(HttpStatus.BAD_REQUEST, "API4001","해당 모드는 존재하지 않습니다.")
+    ;
+
     private final HttpStatus httpStatus;
     private final String code;
     private final String message;
