0c55f37e1de6acdfcf5da3cb156a35eff03c8da2
realisshomyang <realisshoman@kakao.com>
2024-05-23 13:06:51 +0900

    feat : 디렉토리 리마인더용 함수 추가

--- src/main/java/com/linkbrary/domain/reminder/repository/UserDirectoryReminderRepository.java
+++ src/main/java/com/linkbrary/domain/reminder/repository/UserDirectoryReminderRepository.java
@@ -2,10 +2,15 @@ package com.linkbrary.domain.reminder.repository;
 
 import com.linkbrary.domain.directory.entity.UserDirectory;
 import com.linkbrary.domain.reminder.entity.UserDirectoryReminder;
+import com.linkbrary.domain.reminder.entity.UserLinkReminder;
 import com.linkbrary.domain.user.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
 import java.util.List;
 
 @Repository
@@ -13,4 +18,14 @@ public interface UserDirectoryReminderRepository extends JpaRepository<UserDirec
     List<UserDirectoryReminder> findAllByMember(Member member);
     boolean existsByUserDirectory(UserDirectory userDirectory);
 
+
+    @Query(value = "SELECT * FROM user_directory_reminder r " +
+            "JOIN user_directory_reminder_days d ON r.user_directory_reminder_id = d.user_directory_reminder_days_id " +
+            "WHERE d.day_of_week = :currentDay " +
+            "AND r.onoff = true " +
+            "AND r.reminder_time BETWEEN :start AND :end", nativeQuery = true)
+    List<UserDirectoryReminder> findAllByReminderDaysAndReminderTimeBetween(
+            @Param("currentDay") String currentDay,
+            @Param("start") LocalTime start,
+            @Param("end") LocalTime end);
 }
