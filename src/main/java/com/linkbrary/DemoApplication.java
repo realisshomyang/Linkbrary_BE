c723e8710741ee6decd802a3417e0d7fcdea156a
realisshomyang <realisshoman@kakao.com>
2024-05-23 13:05:00 +0900

    feat : 배치 서버를 위한 EnableScheduling 추가

--- src/main/java/com/linkbrary/DemoApplication.java
+++ src/main/java/com/linkbrary/DemoApplication.java
@@ -3,12 +3,16 @@ package com.linkbrary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableRetry
+@EnableScheduling
 public class DemoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 
 }
