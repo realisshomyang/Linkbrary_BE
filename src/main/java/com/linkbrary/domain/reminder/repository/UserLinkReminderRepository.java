abbdca0c54fd18924af49dc0c699fede33747a35
realisshomyang <realisshoman@kakao.com>
2024-05-24 20:09:13 +0900

    feat : 링크 리마인더 푸시 알람용 함수 구현

--- src/main/java/com/linkbrary/domain/reminder/repository/UserLinkReminderRepository.java
+++ src/main/java/com/linkbrary/domain/reminder/repository/UserLinkReminderRepository.java
@@ -5,8 +5,12 @@ import com.linkbrary.domain.link.entity.UserLink;
 import com.linkbrary.domain.reminder.entity.UserLinkReminder;
 import com.linkbrary.domain.user.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
 import java.util.List;
 
 @Repository
@@ -14,4 +18,13 @@ public interface UserLinkReminderRepository extends JpaRepository<UserLinkRemind
     List<UserLinkReminder> findAllByMember(Member member);
     boolean existsByUserLink(UserLink userLink);
 
+    @Query(value = "SELECT * FROM user_link_reminder r " +
+            "JOIN user_link_reminder_days d ON r.user_link_reminder_id = d.user_link_reminder_days_id " +
+            "WHERE d.day_of_week = :currentDay " +
+            "AND r.onoff = true " +
+            "AND r.reminder_time BETWEEN :start AND :end", nativeQuery = true)
+    List<UserLinkReminder> findAllByReminderDaysAndReminderTimeBetween(
+            @Param("currentDay") String currentDay,
+            @Param("start") LocalTime start,
+            @Param("end") LocalTime end);
 }
